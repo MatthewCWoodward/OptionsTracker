3a598d8a184dca6fb3017624bd13c77b21ab01cb
Matthew <matthewoodward01@gmail.com>
2020-11-25 18:42:56 -0500

    added chart tile

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let win;
 
 app.on('ready', () => {
 	win = new BrowserWindow({
-		width: 800,
-		height: 600,
+		minWidth: 800,
+		minHeight: 600,
 		webPreferences: {
 			nodeIntegration: true
 		},
